da6cc285c787884c281c0098c57e73cc3b9d7fe7
Angus Hay <angushay42@gmail.com>
2026-08-11 14:07:01 +0100

    I have a basic program for the TFT display which borrows from the examples given in docs; however, it doesn\'t work. I tried to use UART to print but it was completely garbage, so I thought both were wrong. I have since tested the UART alone and it works fine. Clearly something is wrong, I am getting no data over the actual SPI pins.

--- src/main.c
+++ src/main.c
@@ -3,21 +3,90 @@
 #include "freertos/FreeRTOS.h"
 #include "driver/uart.h"
 
+#include "driver/spi_master.h"
+#include "hal/lcd_types.h"
+#include "esp_lcd_io_spi.h"
+#include "esp_lcd_panel_st7789.h"
+#include "esp_lcd_panel_ops.h"
+
+#include <math.h>
+
+// note: D = MOSI, Q = MISO
+#define MOSI    GPIO_NUM_16
+#define MISO    GPIO_NUM_18
+#define SCLK    GPIO_NUM_17
+#define CS     GPIO_NUM_15
+#define DC     GPIO_NUM_12 // Data Line
+#define RST GPIO_NUM_11
+
+#define LCD_HOST SPI2_HOST  // should be the fast SPI
+#define DOT_CLK_HZ 18 * 1000 * 1000  // I think it is 18MHz, unsure... 
+#define LCD_CMD_BITS 8
+#define LCD_PARAM_BITS 8
+
+#define LCD_H_RES 240
+#define LCD_V_RES 320
+/** it doesn't say this anywhere I can find; 
+ * but, if 18bit color is used then 3 bytes are required. 
+ */
+#define LCD_RGB_BITS 16 // bits
+#define LCD_MAX_LINES 20 // arbitrary number
+#define LCD_MAX_TRANSFER LCD_MAX_LINES * LCD_H_RES * sizeof(uint16_t)
+
+#define UART_PORT 0
+#define UART_TX GPIO_NUM_43
+#define UART_RX GPIO_NUM_44
+
+// todo ?
+// #define SPI_PIN(x) GPIO_NUM_ x
+// SPI_PIN(MOSI);
+
+// todo
+// static hex_to_rgb(const char *hx, int rgb_bits) {
+//     assert(hx != NULL);
+//     if ()
+// }
+
+
+
+static esp_err_t scale_rgb(uint8_t src, uint8_t *dest, size_t size) {
+    assert(dest != NULL);
+    if (size < 5 || size > 6)
+        return ESP_ERR_INVALID_ARG;
+    double r_max, t_max;
+    r_max = (double) (1 << size) - 1;
+    t_max = 255.0;
+    *dest = (uint8_t) round(((double) src * t_max) / r_max);
+
+    return ESP_OK;
+}
+
+/* always returns a 5-6-5 rgb into a uint16_t */
+static esp_err_t encode_rgb(uint8_t r, uint8_t g, uint8_t b, uint16_t *rgb) {
+    assert(rgb != NULL);
+
+    *rgb = 0;
+    ESP_ERROR_CHECK(scale_rgb(r, &r, 5U));
+    ESP_ERROR_CHECK(scale_rgb(g, &g, 6U));
+    ESP_ERROR_CHECK(scale_rgb(b, &b, 5U));
+    *rgb |= r << 11;
+    *rgb |= g << 5;
+    *rgb |= b;
+
+    return ESP_OK;
+}
+
 // prototype
 void blink_test(void);
+void tft_display_test(void);
 
 static bool led = false;
-
 /* NOTE: this is an interrupt context */
 static bool blink_callback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *usr_ctx) {
     
     gpio_set_level(GPIO_NUM_12, (led) ? 1: 0);
     led = !led;
     return false;
-}
-
-void uart_test(void) {
-    
 }
 
 void blink_test(void) {
@@ -60,12 +129,115 @@ void blink_test(void) {
     ESP_ERROR_CHECK(gptimer_register_event_callbacks(blink_gptimer, &blink_callbacks, NULL));
     ESP_ERROR_CHECK(gptimer_enable(blink_gptimer));
     ESP_ERROR_CHECK(gptimer_start(blink_gptimer));
+
+
+    for (;;) {
+        taskYIELD();
+    }   
+
+}
+
+uint16_t frame_buffer[LCD_H_RES * LCD_V_RES];
+
+void tft_display_test_write(esp_lcd_panel_handle_t handle) {
+    for (size_t row = 0; row < (size_t) LCD_H_RES; row += LCD_MAX_LINES) 
+    {
+        ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(
+            handle, 
+            0,
+            row,
+            LCD_V_RES,
+            row + LCD_MAX_LINES,
+            frame_buffer
+        ));
+        
+    }
+}
+
+void tft_display_test(void) {
+    // create an SPI bus
+    spi_bus_config_t bus_cfg = {
+        .sclk_io_num = SCLK,
+        .mosi_io_num = MOSI,
+        .miso_io_num = MISO,
+        .quadhd_io_num = -1,
+        .quadwp_io_num = -1,
+        .max_transfer_sz = LCD_MAX_TRANSFER
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &bus_cfg, SPI_DMA_CH_AUTO));
+
+    // create handle 
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_cfg = {
+        .dc_gpio_num = DC,
+        .cs_gpio_num = CS,
+        .pclk_hz = DOT_CLK_HZ,
+        .lcd_cmd_bits = LCD_CMD_BITS,
+        .lcd_param_bits = LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10 // todo test performance
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(
+        (esp_lcd_spi_bus_handle_t)LCD_HOST, 
+        &io_cfg,
+        &io_handle
+    ));
+
+    // These are required by the ST7789 header file
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_cfg = {
+        .reset_gpio_num = RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = LCD_RGB_BITS
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_cfg, &panel_handle));
+
+
+    uint16_t colour = 0;
+    ESP_ERROR_CHECK(encode_rgb(255U, 0U, 64U, &colour)); 
+    for (size_t row = 0; row < (size_t) LCD_H_RES; row++) {
+        for (size_t col = 0; col < (size_t) LCD_V_RES; col++) {
+            frame_buffer[row * LCD_V_RES + col] = colour;
+        }
+    }
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    for (;;) {
+        tft_display_test_write(panel_handle);
+        taskYIELD();
+    }   
+
+}
+
+void uart_test(void) {
+    uart_config_t uart_cfg = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT
+    };
+    int intr_alloc_flags = 0;   // from echo example
+    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, 1024, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_cfg));
+    ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_PIN_NO_CHANGE, UART_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+    const char *test = "HELLO HELLO HELLO\r\n";
+    for (;;) {
+        uart_write_bytes(UART_PORT, test, strlen(test));
+        taskYIELD();
+    }
 }
 
 void app_main(void) 
 {
-    blink_test();
-    for (;;) {
-        taskYIELD();
-    }   
+    // tft_display_test();
+    uart_test();
 }
