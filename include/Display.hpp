8d58601537af4428629e5cc835ac68f722061581
Angus Hay <angushay42@gmail.com>
2026-09-25 10:23:30 +0100

    1st attempt at incorporating roboeyes, will try a different approach

--- include/Display.hpp
+++ include/Display.hpp
@@ -9,15 +9,16 @@ public:
 
     void init();
 
-
     void setRotation(int rotations);
 };
 
+/// @brief Adapted from TFT_eSPI by Bodmer
 class Sprite {
 public: 
-    Sprite(Display &disp);
+    Sprite(Display *disp);
     void setColorDepth(uint8_t);
-    void createSprite(size_t w, size_t h);
+    void Sprite::drawPixel(int32_t x, int32_t y, uint32_t color);
+    void *createSprite(size_t w, size_t h);
     void deleteSprite();
     void fillSprite(uint16_t color);
     void pushSprite(uint32_t x, uint32_t y);
@@ -25,4 +26,31 @@ public:
     void fillRoundRect(int32_t x, int32_t y, int32_t w, int32_t h, int32_t radius, uint32_t color);
     //                |      corner 1      |       corner 2      |        corner 3      |
     void fillTriangle(int32_t x1,int32_t y1, int32_t x2,int32_t y2, int32_t x3,int32_t y3, uint32_t color);
+private:    
+    Display *_disp;
+protected:
+
+    uint8_t  _bpp;     // bits per pixel (1, 4, 8 or 16)
+    uint16_t *_img;    // pointer to 16-bit sprite
+    uint8_t  *_img8;   // pointer to  1 and 8-bit sprite frame 1 or frame 2
+    uint8_t  *_img4;   // pointer to  4-bit sprite (uses color map)
+    uint8_t  *_img8_1; // pointer to frame 1
+    uint8_t  *_img8_2; // pointer to frame 2
+
+    uint16_t *_colorMap; // color map pointer: 16 entries, used with 4-bit color map.
+
+    int32_t  _sinra;   // Sine of rotation angle in fixed point
+    int32_t  _cosra;   // Cosine of rotation angle in fixed point
+
+    bool     _created; // A Sprite has been created and memory reserved
+    bool     _gFont = false; 
+
+    int32_t  _xs, _ys, _xe, _ye, _xptr, _yptr; // for setWindow
+    int32_t  _sx, _sy; // x,y for scroll zone
+    uint32_t _sw, _sh; // w,h for scroll zone
+    uint32_t _scolor;  // gap fill colour for scroll zone
+
+    int32_t  _iwidth, _iheight; // Sprite memory image bit width and height (swapped during rotations)
+    int32_t  _dwidth, _dheight; // Real sprite width and height (for <8bpp Sprites)
+    int32_t  _bitwidth;         // Sprite image bit width for drawPixel (for <8bpp Sprites, not swapped)
 };
--- src/display.cpp
+++ src/display.cpp
@@ -38,149 +38,322 @@
 
 static const char *TAG = "Display";
 
+
 Display::Display(size_t w, size_t h) 
 {
 
 }
 
-static esp_err_t scale_rgb(uint8_t src, uint8_t *dest, size_t size) {
-    assert(dest != NULL);
-    if (size < 5 || size > 6)
-        return ESP_ERR_INVALID_ARG;
-    volatile double r_max, t_max, temp;
-    r_max = (double) ((1 << size) - 1);
-    t_max = 255.0;
+Display::~Display()
+{
 
-    ESP_LOGD(TAG, "Scaling colour: %i. Reading max=%f, target max=%f.", src, r_max, t_max);
-    temp = round(((double) src * r_max) / t_max);
-    ESP_LOGD(TAG, "Scaled src %u to %f", src, temp);
-    *dest = (uint8_t) temp;
-
-    return ESP_OK;
 }
 
-// todo expand this
-/* always returns a 5-6-5 rgb into a uint16_t */
-static esp_err_t encode_rgb(uint8_t r, uint8_t g, uint8_t b, uint16_t *rgb) {
-    assert(rgb != NULL);
+void Display::init() 
+{
 
-    *rgb = 0;
-    ESP_ERROR_CHECK(scale_rgb(r, &r, 5U));
-    ESP_ERROR_CHECK(scale_rgb(g, &g, 6U));
-    ESP_ERROR_CHECK(scale_rgb(b, &b, 5U));
-    *rgb |= r << 11;
-    *rgb |= g << 5;
-    *rgb |= b;
-
-    return ESP_OK;
 }
 
-uint16_t frame_buffer[LCD_H_RES * LCD_V_RES];
+void Display::setRotation(int rotations)
+{
 
-void tft_display_test_write(esp_lcd_panel_handle_t handle) {
-    ESP_LOGD(TAG, "Writing to display...");
-    for (size_t row = 0; row < (size_t) LCD_V_RES; row += LCD_MAX_LINES) 
-    {
-        ESP_LOGD(TAG, "Writing row %zu...", row);
-        ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(
-            handle, 
-            0,
-            row,
-            LCD_H_RES,
-            row + LCD_MAX_LINES,
-            frame_buffer
-        ));
+}
+
+Sprite::Sprite(Display *disp)
+{
+    _disp = disp;
+
+    _iwidth = 0;
+    _iheight = 0;
+    _bpp = 16;
+
+    _created = false;
+
+    _xs = 0;
+    _ys = 0;
+    _xe = 0;
+    _ye = 0;
+
+    // pushColor coordinates?
+    _xptr = 0;
+    _yptr = 0;
+
+    _colorMap = nullptr;
+}
+
+
+void Sprite::setColorDepth(uint8_t b)
+{
+  // Do not re-create the sprite if the colour depth does not change
+  if (_bpp == b) return _img8_1;
+
+  // Validate the new colour depth
+  if ( b > 8 ) _bpp = 16;  // Bytes per pixel
+  else if ( b > 4 ) _bpp = 8;
+  else if ( b > 1 ) _bpp = 4;
+  else _bpp = 1;
+
+  // Can't change an existing sprite's colour depth so delete and create a new one
+  if (_created) {
+    deleteSprite();
+    return createSprite(_dwidth, _dheight);
+  }
+
+  return nullptr;
+}
+
+void Sprite::drawPixel(int32_t x, int32_t y, uint32_t color)
+{
+  if (!_created || _vpOoB) return;
+
+  x+= _xDatum;
+  y+= _yDatum;
+
+  // Range checking
+  if ((x < _vpX) || (y < _vpY) ||(x >= _vpW) || (y >= _vpH)) return;
+
+  if (_bpp == 16)
+  {
+    color = (color >> 8) | (color << 8);
+    _img[x+y*_iwidth] = (uint16_t) color;
+  }
+  else if (_bpp == 8)
+  {
+    _img8[x+y*_iwidth] = (uint8_t)((color & 0xE000)>>8 | (color & 0x0700)>>6 | (color & 0x0018)>>3);
+  }
+  else if (_bpp == 4)
+  {
+    uint8_t c = color & 0x0F;
+    int index = (x+y*_iwidth)>>1;;
+    if ((x & 0x01) == 0) {
+      _img4[index] = (uint8_t)((c << 4) | (_img4[index] & 0x0F));
     }
+    else {
+      _img4[index] =  (uint8_t)(c | (_img4[index] & 0xF0));
+    }
+  }
+  else // 1 bpp
+  {
+    if (rotation == 1)
+    {
+      uint16_t tx = x;
+      x = _dwidth - y - 1;
+      y = tx;
+    }
+    else if (rotation == 2)
+    {
+      x = _dwidth - x - 1;
+      y = _dheight - y - 1;
+    }
+    else if (rotation == 3)
+    {
+      uint16_t tx = x;
+      x = y;
+      y = _dheight - tx - 1;
+    }
+
+    if (color) _img8[(x + y * _bitwidth)>>3] |=  (0x80 >> (x & 0x7));
+    else       _img8[(x + y * _bitwidth)>>3] &= ~(0x80 >> (x & 0x7));
+  }
 }
 
-void tft_display_test(void) {
-    ESP_LOGI(TAG, "Creating bus config...");
-    // create an SPI bus
-    spi_bus_config_t bus_cfg = {
-        .sclk_io_num = SCLK,
-        .mosi_io_num = MOSI,
-        .miso_io_num = -1,
-        .quadhd_io_num = -1,
-        .quadwp_io_num = -1,
-        .max_transfer_sz = LCD_MAX_TRANSFER,
-    };
-    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &bus_cfg, SPI_DMA_CH_AUTO));
 
-    ESP_LOGI(TAG, "Creating panel handle...");
-    // create handle 
-    esp_lcd_panel_io_handle_t io_handle = NULL;
-    esp_lcd_panel_io_spi_config_t io_cfg = {
-        .dc_gpio_num = DC,
-        .cs_gpio_num = CS,
-        .pclk_hz = DOT_CLK_HZ,
-        .lcd_cmd_bits = LCD_CMD_BITS,
-        .lcd_param_bits = LCD_PARAM_BITS,
-        .spi_mode = 0,
-        .trans_queue_depth = 10 // todo test performance
-    };
+void *Sprite::createSprite(size_t w, size_t h)
+{
+    if ( _created ) 
+        return _img8_1;
 
-    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(
-        (esp_lcd_spi_bus_handle_t)LCD_HOST, 
-        &io_cfg,
-        &io_handle
-    ));
+    if ( w < 1 || h < 1 ) 
+        return nullptr;
 
-    ESP_LOGI(TAG, "Creating ST7789 panel...");
-    // These are required by the ST7789 header file
-    esp_lcd_panel_handle_t panel_handle = NULL;
-    esp_lcd_panel_dev_config_t panel_cfg = {
-        .reset_gpio_num = RST,
-        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
-        .bits_per_pixel = LCD_RGB_BITS,
-        .data_endian = LCD_RGB_DATA_ENDIAN_LITTLE
-    };
+    _iwidth  = _dwidth  = _bitwidth = w;
+    _iheight = _dheight = h;
 
+    cursor_x = 0;
+    cursor_y = 0;
 
-    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_cfg, &panel_handle));
+    // Default scroll rectangle and gap fill colour
+    _sx = 0;
+    _sy = 0;
+    _sw = w;
+    _sh = h;
+    _scolor = TFT_BLACK;
 
-    // ESP_LOGI(TAG, "Encoding RGB...");
-    // uint16_t colour = 0;
-    // ESP_ERROR_CHECK(encode_rgb(0, 0, 0, &colour)); 
-    // ESP_LOGD(TAG, "Colour is: %u", colour);
+    _img8   = (uint8_t*) callocSprite(w, h, frames);
+    _img8_1 = _img8;
+    _img8_2 = _img8;
+    _img    = (uint16_t*) _img8;
+    _img4   = _img8;
 
-    uint16_t test_colour;
+    if ( (_bpp == 16) && (frames > 1) ) 
+    {
+        _img8_2 = _img8 + (w * h * 2 + 1);
+    }
 
-    ESP_ERROR_CHECK(encode_rgb(255,0,0, &test_colour));
+    if ( (_bpp == 8) && (frames > 1) ) 
+    {
+        _img8_2 = _img8 + (w * h + 1);
+    }
 
-    ESP_LOGD(TAG, "Filling frame buffer...");
-    for (size_t row = 0; row < (size_t) LCD_V_RES; row++) {
-        for (size_t col = 0; col < (size_t) LCD_H_RES; col++) {
-            frame_buffer[row * LCD_H_RES + col] = test_colour;
+    // This is to make it clear what pointer size is expected to be used
+    // but casting in the user sketch is needed due to the use of void*
+    if ( (_bpp == 1) && (frames > 1) )
+    {
+        w = (w+7) & 0xFFF8;
+        _img8_2 = _img8 + ( (w>>3) * h + 1 );
+    }
+
+    if (_img8)
+    {
+        _created = true;
+        if ( (_bpp == 4) && (_colorMap == nullptr)) createPalette(default_4bit_palette);
+
+        rotation = 0;
+        setViewport(0, 0, _dwidth, _dheight);
+        setPivot(_iwidth/2, _iheight/2);
+        return _img8_1;
+    }
+
+    return nullptr;
+}
+void Sprite::deleteSprite()
+{
+  if (_colorMap != nullptr)
+  {
+    free(_colorMap);
+    _colorMap = nullptr;
+  }
+
+  if (_created)
+  {
+    free(_img8_1);
+    _img8 = nullptr;
+    _created = false;
+    _vpOoB   = true;  // TFT_eSPI class write() uses this to check for valid sprite
+  }
+}
+
+void Sprite::fillSprite(uint16_t color)
+{
+
+}
+void Sprite::pushSprite(uint32_t x, uint32_t y)
+{
+
+}
+void Sprite::fillRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t color)
+{
+    if (!_created || _vpOoB) return;
+
+    x+= _xDatum;
+    y+= _yDatum;
+
+    // Clipping
+    if ((x >= _vpW) || (y >= _vpH)) return;
+
+    if (x < _vpX) { w += x - _vpX; x = _vpX; }
+    if (y < _vpY) { h += y - _vpY; y = _vpY; }
+
+    if ((x + w) > _vpW) w = _vpW - x;
+    if ((y + h) > _vpH) h = _vpH - y;
+
+    if ((w < 1) || (h < 1)) return;
+
+    int32_t yp = _iwidth * y + x;
+
+    if (_bpp == 16)
+    {
+        color = (color >> 8) | (color << 8);
+        uint32_t iw = w;
+        int32_t ys = yp;
+        if(h--)  {while (iw--) _img[yp++] = (uint16_t) color;}
+        yp = ys;
+        while (h--)
+        {
+        yp += _iwidth;
+        memcpy( _img+yp, _img+ys, w<<1);
         }
     }
+    else if (_bpp == 8)
+    {
+        color = (color & 0xE000)>>8 | (color & 0x0700)>>6 | (color & 0x0018)>>3;
+        while (h--)
+        {
+        memset(_img8 + yp, (uint8_t)color, w);
+        yp += _iwidth;
+        }
+    }
+    else if (_bpp == 4)
+    {
+        uint8_t c1 = (uint8_t)color & 0x0F;
+        uint8_t c2 = c1 | ((c1 << 4) & 0xF0);
+        if ((x & 0x01) == 0 && (w & 0x01) == 0)
+        {
+        yp = (yp >> 1);
+        while (h--)
+        {
+            memset(_img4 + yp, c2, (w>>1));
+            yp += (_iwidth >> 1);
+        }
+        }
+        else if ((x & 0x01) == 0)
+        {
 
-    ESP_LOGI(TAG, "Starting panel up...");
-    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
-
-    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
-
-    // ESP_LOGD(TAG, 
-    //     "colour is: \t%u. \n
-    //     First half: \t%u, \n
-    //     Second half: \t%u \n", 
-    //     colour, 
-    //     *(uint8_t *) &colour,
-    //     *((uint8_t *) &colour + 1)
-    // );
-
-    ESP_LOGI(TAG, "Beginning write sequence...");
-    for (;;) {
-        
-        tft_display_test_write(panel_handle);
-        taskYIELD();
-    }   
-
+        // same as above but you have a hangover on the right.
+        yp = (yp >> 1);
+        while (h--)
+        {
+            if (w > 1)
+            memset(_img4 + yp, c2, (w-1)>>1);
+            // handle the rightmost pixel by calling drawPixel
+            drawPixel(x+w-1-_xDatum, y+h-_yDatum, c1);
+            yp += (_iwidth >> 1);
+        }
+        }
+        else if ((w & 0x01) == 1)
+        {
+        yp = (yp + 1) >> 1;
+        while (h--) {
+            drawPixel(x-_xDatum, y+h-_yDatum, color & 0x0F);
+            if (w > 1)
+            memset(_img4 + yp, c2, (w-1)>>1);
+            // same as above but you have a hangover on the left instead
+            yp += (_iwidth >> 1);
+        }
+        }
+        else
+        {
+        yp = (yp + 1) >> 1;
+        while (h--) {
+            drawPixel(x-_xDatum, y+h-_yDatum, color & 0x0F);
+            if (w > 1) drawPixel(x+w-1-_xDatum, y+h-_yDatum, color & 0x0F);
+            if (w > 2)
+            memset(_img4 + yp, c2, (w-2)>>1);
+            // maximal hacking, single pixels on left and right.
+            yp += (_iwidth >> 1);
+        }
+        }
+    }
+    else
+    {
+        x -= _xDatum;
+        y -= _yDatum;
+        while (h--)
+        {
+        int32_t ww = w;
+        int32_t xx = x;
+        while (ww--) drawPixel(xx++, y, color);
+        y++;
+        }
+    }
 }
 
-void app_main(void) 
+void Sprite::fillRoundRect(int32_t x, int32_t y, int32_t w, int32_t h, int32_t radius, uint32_t color)
 {
-    ESP_LOGI(TAG, "Beginning test...");
-    tft_display_test();
+
+}
+//                |      corner 1      |       corner 2      |        corner 3      |
+void Sprite::fillTriangle(int32_t x1,int32_t y1, int32_t x2,int32_t y2, int32_t x3,int32_t y3, uint32_t color)
+{
+
 }
